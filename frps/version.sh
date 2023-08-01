91d473c5acbb6a4ec1a4635ca3f84f6a8e36cf1b
封尘半烟 <jsvip7844@qq.com>
2023-08-01 14:27:10 +0800

    frps v0.51.2

---
+++ frps/version.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export FRPS_VER=0.51.2
+export FRPS_INIT="https://raw.githubusercontent.com/jsgzsjskj/Frp-install/master/frps/frps.init"
+export github_download_url="https://github.com/fatedier/frp/releases/download"
