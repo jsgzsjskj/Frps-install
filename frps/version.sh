fbf186e60975d3500c2d08e254a706a98cf6c0b2
封尘半烟 <jsvip7844@qq.com>
2023-08-01 14:35:22 +0800

    Delete frps directory

--- frps/version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export FRPS_VER=0.51.2
-export FRPS_INIT="https://raw.githubusercontent.com/jsgzsjskj/Frp-install/master/frps/frps.init"
-export github_download_url="https://github.com/fatedier/frp/releases/download"
