f97cad5241903593f06a72d258514139fbcf1e69
封尘半烟 <jsvip7844@qq.com>
2023-08-01 15:42:58 +0800

    Update version.sh

--- frps/version.sh
+++ frps/version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export FRPS_VER=0.51.2
+export FRPS_VER=v0.51.2
 export FRPS_INIT="https://raw.githubusercontent.com/jsgzsjskj/Frps-install/master/frps/frps.init"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
