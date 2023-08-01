662d74381abdc361a2d93d8009f9e3df44622501
封尘半烟 <jsvip7844@qq.com>
2023-08-01 14:17:08 +0800

    Add files via upload

---
+++ frp/version.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export FRPS_VER=0.51.2
+export FRPS_INIT="https://raw.githubusercontent.com/clangcn/onekey-install-shell/master/frps/frps.init"
+export github_download_url="https://github.com/fatedier/frp/releases/download"
